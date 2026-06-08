a78cb6ed8b47e1bff1301597fb0f34b89c732d07
Gordon Messmer <gmessmer@redhat.com>
2026-06-07 22:36:47 -0700

    Install podman-desktop from COPR

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,9 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 -y copr enable gordonmessmer/nodejs-electron
+dnf5 -y install podman-desktop
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
